987e87ad644dbe726230d4bce1adc764186a32bc
GarrettMeyer1 <garrett.meyer1@marist.edu>
2016-09-07 21:43:30 -0400

    Lab 1.

---
+++ problem2_5.java
@@ -0,0 +1,33 @@
+/**
+*file: Problem2_5.java
+*author: Garrett Meyer
+*course: CMPT 220
+*assignment: Problem2.5
+*due date: September 7
+*version: 1.8
+*
+*The file "Problem 2.5" is a program that reads the subtotal and gratuity rate, then compares the two
+*/
+import java.util.Scanner;
+public class problem2_5 {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		double	subtotal,
+				gratuityRate,
+				tip,
+				total;
+
+	
+		System.out.print("Please enter the subtotal and the gratuity rate: ");
+		subtotal = input.nextDouble();
+		gratuityRate = input.nextDouble();
+
+		tip = subtotal + (gratuityRate * .01);
+		total = subtotal + tip;
+
+		System.out.print("The total is $" + total + "The gratuity is $" + tip + "The subtotal is $" + subtotal);
+	}
+}
---
+++ problem2_6.java
@@ -0,0 +1,33 @@
+/**
+*file:Problem 2.6.java
+*author: Garrett Meyer
+*course: CMPT 220
+*assignment: Problem 2.6
+*due date: September 7
+*version: 1.8
+*
+*The file "Problem 2.6" reads an integer between 0 and 1000 
+*and adds all the digits in the integer
+*/
+import java.util.Scanner;
+
+public class problem2_6 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter an integer between 0 and 1000");
+		int number = input.nextInt();
+
+		int firstDigit = number % 10;
+		int remainingNumber = number / 10;
+		int secondDigit = remainingNumber % 10;
+			remainingNumber = remainingNumber / 10;
+		int thirdDigit = remainingNumber % 10;
+
+		int sum = firstDigit + secondDigit + thirdDigit;
+
+		System.out.println("The sum of all digits in " + number + "is" + sum);
+	}
+}
